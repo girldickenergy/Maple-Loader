a87374a7c84511d4efd436ac15eb8c8fa79384e9
mrflashstudio <mrflashstudio@gmail.com>
2021-10-30 21:38:31 +0300

    oops

--- Maple Loader/Packets/Responses/LoginResponse.cpp
+++ Maple Loader/Packets/Responses/LoginResponse.cpp
@@ -39,8 +39,8 @@ LoginResponse::LoginResponse(const char* msg, size_t size, MatchedClient* matche
 	decryptedLoginResultSplit[2].erase(decryptedLoginResultSplit[2].begin());
 	decryptedLoginResultSplit[3].erase(decryptedLoginResultSplit[3].begin());
 	SessionToken = decryptedLoginResultSplit[1];
-	DiscordID = decryptedLoginResultSplit[1];
-	AvatarHash = decryptedLoginResultSplit[1];
+	DiscordID = decryptedLoginResultSplit[2];
+	AvatarHash = decryptedLoginResultSplit[3];
 	matchedClient->sessionToken = SessionToken;
 	matchedClient->discordID = DiscordID;
 	matchedClient->avatarHash = AvatarHash;
