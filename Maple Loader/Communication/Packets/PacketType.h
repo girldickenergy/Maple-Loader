a6aa18b93866e96ecbf885300c2ea124b510f0df
mrflashstudio <mrflashstudio@gmail.com>
2022-11-03 10:05:04 +0300

    remove unused packet types

--- Maple Loader/Communication/Packets/PacketType.h
+++ Maple Loader/Communication/Packets/PacketType.h
@@ -4,8 +4,5 @@ enum class PacketType : unsigned char
 {
     Handshake = 0xA0,
     Login = 0xB0,
-    LoaderStream = 0xC0,
-    ImageStream_StageOne = 0xD0,
-    ImageStream_StageTwo = 0xB1,
-    Heartbeat = 0xE0
+    LoaderStream = 0xC0
 };
